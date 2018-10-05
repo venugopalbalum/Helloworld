0301ec084d696d626e196b17f595a04b70e65db9
Balum <venugopal.balum@accenture.com>
2018-10-05 13:02:27 +0530

    Title Added

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -2,7 +2,6 @@ package org.ravi.helloworld;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
-import static org.testng.Assert.assertTrue;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -16,23 +15,23 @@ import org.testng.annotations.AfterTest;
 public class HelloTest {
 
 	WebDriver driver;
-	WebElement text;
+	WebElement text; 
 	@BeforeTest
 	public void beforeTest() {
 		/*	System.setProperty("webdriver.gecko.driver", "resources\\geckodriver32.exe");
-		driver = new FirefoxDriver();
+		driver = new FirefoxDriver();*/
 		System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("http://localhost:8080/HelloWorld.war/");
-		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);*/
+		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
 	}
 
 	@Test
 	public void f() {
 
-		/*text = driver.findElement(By.xpath("html/body/h2"));
+		//text = driver.findElement(By.xpath("html/body/h2"));
 		Assert.assertEquals(driver.getTitle(), "Hello World");
-		System.out.println("Test Passed");*/
+		System.out.println("Test Passed");
 
 	}
 
